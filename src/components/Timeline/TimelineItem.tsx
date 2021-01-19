1a30782f820a04c689ec6e511e559cd6a7309161
EregarMK <mkoscianek1@gmail.com>
2021-01-19 15:06:53 +0100

    fix overflow

--- src/components/Timeline/TimelineItem.tsx
+++ src/components/Timeline/TimelineItem.tsx
@@ -9,14 +9,14 @@ type TimelineItemProps = {
 }
 
 const TimelineItem = ({right, children, label}: TimelineItemProps) => {
-    const [isAnimated, onAnimation] = useState(false)
+    const [inView, handleInView] = useState(false)
     const isMobile = window.innerWidth < 768
-    
+
     return (
-        <InView onChange={(inView, entry) => onAnimation(inView)}>
-            <div className={`timeline-item-wrapper timeline-item-wrapper-${(right || isMobile) ? 'right' : ''}`}>
-                <div className={`timeline-item-icon icon-suitcase ${isAnimated ? 'pop' : ''}`}></div>
-                <div className={`timeline-item-content ${isAnimated ? `slideIn-${(right || isMobile) ? 'right' : 'left'}` : ''}`}>
+        <InView onChange={(inView, entry) => handleInView(inView)}>
+            <div className={`timeline-item-wrapper ${(right || isMobile) ? 'timeline-item-wrapper-right' : ''}`}>
+                <div className={`timeline-item-icon icon-suitcase ${inView ? 'pop' : 'hide'}`}></div>
+                <div className={`timeline-item-content ${inView ? `slideIn-${(right || isMobile) ? 'right' : 'left'}` : 'hide'}`}>
                     {children}
                     <div className='timeline-label-wrapper'><div className='timeline-label-content'>{label}</div></div>
                     <div className='timeline-item-arrow' />
